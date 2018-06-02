fb8b4a470095041198ad985592e6ec2c874e251f
Ruslan <ruslan.arifullin@gmail.com>
2018-06-02 23:49:23 +0300

    attending workshops

---
+++ cpp/12_attending_workshops.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+
+/*
+Developer:   Ruslan Arifullin
+Date:        JUN 01 2018
+             
+DIFFICULTY:  Medium
+
+PROBLEM:     
+A student signed up for workshops and wants to attend the maximum number of workshops where no two workshops overlap. You must do the following:
+
+Implement structures:
+
+    struct Workshop having the following members:
+        The workshop's start time.
+        The workshop's duration.
+        The workshop's end time.
+
+    struct Available_Workshops having the following members:
+        An integer, (the number of workshops the student signed up for).
+        An array of type Workshop array having size .
+
+Implement functions:
+
+    Available_Workshops* initialize (int start_time[], int duration[], int n)
+    Creates an Available_Workshops object and initializes its elements using the elements in the and parameters (both are of size ). Here, and are the respective start time and duration for the workshop. This function must return a pointer to an Available_Workshops object.
+
+    int CalculateMaxWorkshops(Available_Workshops* ptr)
+    Returns the maximum number of workshops the student can attend—without overlap. The next workshop cannot be attended until the previous workshop ends.
+
+Sample Input
+
+6
+1 3 0 5 5 8
+1 1 6 2 4 1
+
+Sample Output 4
+    
+*/
+
+using namespace std;
+
+    struct Workshop {
+        int start_time;
+        int duration;
+        int end_time;
+    };
+    struct Available_Workshops {
+        int n;
+        vector<Workshop> workshop;
+    };
+
+bool compare(Workshop w1, Workshop w2) {
+    return w1.end_time < w2.end_time;
+}
+
+Available_Workshops* initialize(int* start_time, int* duration, int n) {
+    Available_Workshops* aw = new Available_Workshops();
+
+    aw->n = n;
+    for (int i = 0; i != n; ++i) {
+        Workshop w;
+        w.start_time = start_time[i];
+        w.duration = duration[i];
+        w.end_time = start_time[i] + duration[i];
+        aw->workshop.push_back(w);
+    }
+
+    return aw;
+}
+
+int CalculateMaxWorkshops(Available_Workshops* ptr) {
+    int lastTime = 0;
+    int maxWorkshops = 0;
+
+    sort(ptr->workshop.begin(), ptr->workshop.end(), compare);
+    for (auto& it : ptr->workshop) {
+        if (it.start_time >= lastTime){
+            maxWorkshops++;
+            lastTime = it.end_time;
+        }
+    }
+
+    return maxWorkshops;
+}
+
+int main(int argc, char *argv[]) {
+    int n; // number of workshops
+    cin >> n;
+    // create arrays of unknown size n
+    int* start_time = new int[n];
+    int* duration = new int[n];
+
+    for(int i=0; i < n; i++){
+        cin >> start_time[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> duration[i];
+    }
+
+    Available_Workshops * ptr;
+    ptr = initialize(start_time,duration, n);
+    cout << CalculateMaxWorkshops(ptr) << endl;
+    return 0;
+}
+
